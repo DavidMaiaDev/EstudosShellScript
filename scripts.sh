f722edf4d2dfa751d0365e7eef1990a4692c1ba5
DavidMaiaDev <david.goncalves@dcx.ufpb.br>
2024-06-11 22:55:41 -0300

    Adicionar novo script

---
+++ scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#criado por David Maia
+
+segundos="$1"
+
+echo "."
+sleep $segundos
+echo ".."
+sleep $segundos
+echo "..."
+sleep $segundos
+echo "...."
+sleep $segundos
+echo "....."
